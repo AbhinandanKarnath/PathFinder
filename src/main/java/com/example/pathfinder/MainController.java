ce190c6cc9a703a986ddefb2b2e41a4c972e4081
noobmaster52 <abhinandankaranth52@gmail.com>
2023-10-30 15:59:39 +0530

    Created a grid and made it react to some mouse events

--- src/main/java/com/example/pathfinder/MainController.java
+++ src/main/java/com/example/pathfinder/MainController.java
@@ -1,15 +1,52 @@
 package com.example.pathfinder;
 
 import javafx.fxml.FXML;
-import javafx.scene.canvas.Canvas;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 
-public class HelloController {
+import java.util.EventListener;
+
+public class MainController {
     @FXML
-    Canvas grid1;
+    AnchorPane grid1;
 
     @FXML
-    public void onOver()
+    void initialize()
     {
 
+        for(int i = 0 ; i< 18 ; i++)
+        {
+            for( int j = 0 ; j < 21 ;j++)
+            {
+                StackPane sp = new StackPane();
+
+                sp.setPrefWidth(20);
+                sp.setPrefHeight(20);
+
+                sp.setOnMouseEntered(mouseEvent -> sp.setStyle("-fx-background-color: white;"));
+//                sp.setOnMouseExited(mouseEvent -> onOver(sp));
+                sp.setOnMouseClicked(mouseEvent -> onClick(sp));
+
+                AnchorPane.setLeftAnchor(sp, j*23.0);
+                AnchorPane.setTopAnchor(sp, i*20.0);
+                sp.setStyle("-fx-background-color: lightgray;");
+                grid1.getChildren().add(sp);
+            }
+        }
+
+    }
+
+    @FXML
+    public void onOver(StackPane sp)
+    {
+         sp.setStyle("-fx-background-color: black;");
+    }
+
+    @FXML
+    public  void onClick(StackPane cell)
+    {
+        cell.setStyle("-fx-background-color: red");
     }
 }
